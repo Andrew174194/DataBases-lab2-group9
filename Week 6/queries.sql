a8f167a0834e6fa3c4fd872b84f5219efaabd21e
ox7a59 <ox7a59@gmail.com>
2021-03-02 07:16:29 -0500

    week06.

--- Week 6/queries.sql
+++ Week 6/queries.sql
@@ -5,7 +5,8 @@ explain (verbose, analyze) select title from film where ((rating='R' or rating='
 explain (verbose, analyze) select store_id, sum(amount) as "money" from payment left outer join customer using (customer_id) left outer join store using (store_id) where payment_date > '2007/04/14' and payment_date < '2007/05/14' group by store_id;
 
 -- solution for first query:
-?
+create index on inventory using btree (inventory_id, film_id);
+create index on film_category using btree (category_id, film_id);
 
 -- solution for second query:
 create index on payment using btree(payment_date);
