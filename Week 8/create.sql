aa08e21b0124581b9ef0c8cebfed3b357dfe5620
ox7a59 <ox7a59@gmail.com>
2021-03-23 09:26:55 +0300

    update.

--- Week 8/create.sql
+++ Week 8/create.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS accounts (
    account_name VARCHAR ( 255 ) UNIQUE NOT NULL,
    account_credit integer NOT NULL
 );
+
+
+insert into accounts(account_name, account_credit) values ('Vladimir', 1000);
+
+insert into accounts(account_name, account_credit) values ('German', 1000);
+
+insert into accounts(account_name, account_credit) values ('Andrey', 1000);
