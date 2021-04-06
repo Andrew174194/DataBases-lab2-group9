bb17682a8fb71923cf2704ccdb7fa4564070549a
ox7a59 <ox7a59@gmail.com>
2021-04-06 04:40:25 -0400

    update.

---
+++ Week 12/second_repeatable.sql
@@ -0,0 +1,53 @@
+-- REPEATABLE READ
+
+-- step I
+-- terminal 1
+begin;
+set transaction isolation level repeatable read;
+
+-- terminal 2
+begin;
+set transaction isolation level repeatable read;
+
+
+-- step 2
+-- terminal 1
+select * from test_table where group_id = 2;
+
+ id | username |   fullname   | balance | group_id 
+----+----------+--------------+---------+----------
+  3 | mike     | Michael Dole |      73 |        2
+(1 row)
+
+-- step 3
+-- terminal 2
+update test_table set group_id = 2 where username = 'bbrown';
+
+-- step 4
+-- terminal 1
+select * from test_table where group_id = 2;
+ id | username |   fullname   | balance | group_id 
+----+----------+--------------+---------+----------
+  3 | mike     | Michael Dole |      73 |        2
+(1 row)
+
+-- so i read only previous data
+
+-- step 5
+-- terminal 1
+update test_table set balance = balance + 15 where group_id = 2;
+
+-- step 6
+-- terminal 1 && 2
+commit;
+select * from test_table;
+ id | username |     fullname     | balance | group_id 
+----+----------+------------------+---------+----------
+  1 | jones    | Alice Jones      |      82 |        1
+  2 | bitdiddl | Ben Bitdiddle    |      65 |        1
+  4 | alyssa   | Alyssa P. Hacker |      79 |        3
+  5 | bbrown   | Bob Brown        |     100 |        2
+  3 | mike     | Michael Dole     |      88 |        2
+(5 rows)
+
+-- so we do not update Bob's balance
