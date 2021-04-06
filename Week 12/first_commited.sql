50e762e4f5fb33bf9ccff4a5ca6b1ccf06fc377a
ox7a59 <ox7a59@gmail.com>
2021-04-06 03:42:35 -0400

    update.

--- Week 12/first_commited.sql
+++ Week 12/first_commited.sql
@@ -1,3 +1,5 @@
+-- READ COMMITED
+
 -- step I
 -- terminal 1
 begin;
@@ -9,6 +11,8 @@ select * from test_table;
 begin;
 set transaction isolation level read committed;
 update test_table set username = 'ajones' where fullname = 'Alice Jones';
+
+-- we're not hang here because we're reading commited data
 
 -- step III
 -- terminal 1 output:
@@ -46,3 +50,41 @@ postgres=# select * from test_table;
   1 | ajones   | Alice Jones      |      82 |        1
 (5 rows)
 
+-- step VI
+-- terminal 2
+begin;
+set transaction isolation level read committed;
+savepoint T1;
+
+-- step VII
+-- terminal 1
+begin;
+set transaction isolation level read committed;
+update test_table set balance = balance + 10 where fullname = 'Alice Jones';
+
+-- step VIII
+-- terminal 2
+begin;
+set transaction isolation level read committed;
+update test_table set balance = balance + 20 where fullname = 'Alice Jones';
+
+-- we're hang on terminal 2, waiting for commit on first terminal
+
+-- step IX
+-- terminal 1
+commit;
+
+-- step X
+-- terminal 2
+rollback to T1;
+
+-- terminal 1 && 2 output:
+postgres=# select * from test_table;
+ id | username |     fullname     | balance | group_id 
+----+----------+------------------+---------+----------
+  2 | bitdiddl | Ben Bitdiddle    |      65 |        1
+  3 | mike     | Michael Dole     |      73 |        2
+  4 | alyssa   | Alyssa P. Hacker |      79 |        3
+  5 | bbrown   | Bob Brown        |     100 |        3
+  1 | ajones   | Alice Jones      |      92 |        1
+(5 rows)
