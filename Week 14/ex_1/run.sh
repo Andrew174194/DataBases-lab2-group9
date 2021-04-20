9b740d2c56b4af223e8ec28d01c1bf5c910a2bb6
ox7a59 <ox7a59@gmail.com>
2021-04-20 11:30:18 +0300

    update.

--- Week 14/ex_1/run.sh
+++ Week 14/ex_1/run.sh
@@ -2,3 +2,14 @@
 redis-cli flushall
 cat customers.txt | redis-cli --pipe
 cat orders.txt | redis-cli --pipe
+echo 'First customer:'
+redis-cli HMGET customers:1 customer_numb first_name last_name
+echo
+echo 'Fourth customer:'
+redis-cli HMGET customers:4 customer_numb first_name last_name
+echo
+echo 'First order:'
+redis-cli HMGET orders:1 order_numb customer_numb order_date order_total
+echo
+echo 'Fourth order:'
+redis-cli HMGET orders:4 order_numb customer_numb order_date order_total
--- Week 14/ex_2/run.sh
+++ Week 14/ex_2/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 redis-cli flushall
 cat profiles.txt | redis-cli --pipe
+echo 'First user:'
+redis-cli HMGET profiles:1 id username name followers following posts
+echo
+
+echo 'First post:'
+redis-cli HMGET posts:1 post_id id timestamp text
